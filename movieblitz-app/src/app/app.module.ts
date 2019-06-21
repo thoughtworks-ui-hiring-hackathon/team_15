547bab6b2d3c2f198ddefde14940b487bbfcfbfa
akshaykinhikar <kinhikar.akshay32@gmail.com>
2019-06-21 11:59:33 +0530

    nav and home components added

--- movieblitz-app/src/app/app.module.ts
+++ movieblitz-app/src/app/app.module.ts
@@ -2,11 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app-component/app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
